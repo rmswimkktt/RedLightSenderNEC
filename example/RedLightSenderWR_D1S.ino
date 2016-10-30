372d782fc0d7dfa9a36f5b9999b449d215e5d30f
Daiki Maruta <rmswimkktt@gmail.com>
2016-10-30 23:58:02 +0900

    固定コマンドの使用例を作成

--- example/RedLightSenderWR_D1S.ino
+++ example/RedLightSenderWR_D1S.ino
@@ -2,7 +2,7 @@
 // WR_D1Sフォーマットの赤外線を照射する
 //4D
 //01001101
-//82
+//B2
 //10110010
 //40
 //01000000
@@ -14,7 +14,7 @@
 //10011100
 
 // 送信データ
-const unsigned int DATA_CODE[3] = {0x4D82, 0x40BF, 0x639C};
+const unsigned int DATA_CODE[3] = {0x4DB2, 0x40BF, 0x639C};
 
 RedLightSenderWR_D1S red(12);
 
@@ -32,7 +32,16 @@ void setup(){
 }
 
 void loop(){
-  red.sendData(DATA_CODE);
+  //Serial.println("Send!");
+  //red.sendData(DATA_CODE);
 
-  delay(10000);
+  delay(1000);
+  Serial.println("HEAT");
+  red.onHeating();
+  delay(1000);
+  Serial.println("COOL");
+  red.onCooling();
+  delay(1000);
+  Serial.println("OFF");
+  red.off();
 }
